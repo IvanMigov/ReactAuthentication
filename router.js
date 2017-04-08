25bc1a05c0ecdb25cdde8a6b1e261ea9b8903d16
ivan <ivan.migov@gmail.com>
2017-04-08 20:41:04 +0300

    mongo db is working

--- router.js
+++ router.js
@@ -1,5 +1,9 @@
+const Athentication = require('./controllers/authentication');
+
+
 module.exports = function (app) {
-  app.get('/',function (req, res, next) {
-    res.send(['water','phone','paper']);
-  });
+  // app.get('/',function (req, res, next) {
+  //   res.send(['water','phone','paper']);
+  // });
+  app.post('/signup',Athentication.signup);
 };
